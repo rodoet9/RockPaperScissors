997635322bb11086cd3da52a1e223a7f1590e691
rodoet9 <102057385+rodoet9@users.noreply.github.com>
2022-07-17 22:49:44 +0300

    fixed an error at counting the score

--- app.js
+++ app.js
@@ -12,68 +12,72 @@ let userScore = 0;
 
 let userChoice;
 let computerChoice;
-let result; 
+let result;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-userChoice = e.target.id;
-userChoiceDisplay.innerHTML = userChoice;
-generateComputerChoice();
-getResult();
-numberOfGames();
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
+    numberOfGames();
 }))
 
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
     console.log(randomNumber)
 
-    if(randomNumber === 1) {
+    if (randomNumber === 1) {
         computerChoice = '👊';
     }
-    if(randomNumber === 2) { 
+    if (randomNumber === 2) {
         computerChoice = '✂️';
     }
-    if(randomNumber === 3) {
+    if (randomNumber === 3) {
         computerChoice = '✋';
     }
     computerChoiceDisplay.innerHTML = computerChoice;
 }
-function getResult () {
-    if(computerChoice === userChoice) {
+function getResult() {
+    if (computerChoice === userChoice) {
         result = 'It\'s a tie';
     }
-    if(computerChoice === '👊' && userChoice === '✋') {
+    if (computerChoice === '👊' && userChoice === '✋') {
         result = 'Player wins';
     }
-    if(computerChoice === '👊' && userChoice === '✂️') {
+    if (computerChoice === '👊' && userChoice === '✂️') {
         result = 'Computer wins';
     }
-    if(computerChoice === '✋' && userChoice === '✂️') {
+    if (computerChoice === '✋' && userChoice === '✂️') {
         result = 'Player wins';
     }
-    if(computerChoice === '✋' && userChoice === '👊') {
+    if (computerChoice === '✋' && userChoice === '👊') {
         result = 'Computer wins';
     }
-    if(computerChoice === '✂️' && userChoice === '✋') {
+    if (computerChoice === '✂️' && userChoice === '✋') {
         result = 'Computer wins';
     }
-    if(computerChoice === '✂️' && userChoice === '👊') {
+    if (computerChoice === '✂️' && userChoice === '👊') {
         result = 'Player wins';
     }
     resultDisplay.innerHTML = result;
 }
 
-function numberOfGames () {
-    if( result === 'Computer wins') {
-        computerScore++;
-        computerScoreDisplay.innerHTML = computerScore;
-    } else {
+function numberOfGames() {
+    if (result === 'It\'s a tie') {
+        userScore += 0;
+        computerScore += 0;
+    } if (result === 'Player wins') {
         userScore++;
         userScoreDisplay.innerHTML = userScore;
     }
-    if( computerScore == 5 ) {
+    if (result === 'Computer wins') {
+        computerScore++;
+        computerScoreDisplay.innerHTML = computerScore;
+    }
+    if (computerScore == 5) {
         result = 'The winner of this round is the computer';
         resultDisplay.innerHTML = result;
-    } if( userScore == 5 ) {
+    } if (userScore == 5) {
         result = 'Congrats! You are the winner of this round.';
-    }   resultDisplay.innerHTML = result;
+    } resultDisplay.innerHTML = result;
 }
